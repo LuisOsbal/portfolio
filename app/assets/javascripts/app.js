4a0c3e87b2f400b60bf9ff552c967c4053280ea7
Luis Osnet <osnet@outlook.com>
2018-05-11 13:53:40 -0500

    responsive navbar

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,16 +1,20 @@
-$("ul").addClass('menu-list').not(':first').addClass('is-closed');
-$(".is-closed").siblings('a').addClass('menu-has-children has-closed');
-
-$("a.menu-has-children").on('click', function(){
-  var firstUL = $(this).siblings('ul');
-  if(firstUL.hasClass('is-closed'))
-  {
-    $(this).removeClass('has-closed');
-    firstUL.removeClass('is-closed');
-  }
-  else
-  {
-    $(this).addClass('has-closed');
-    firstUL.addClass('is-closed');
+document.addEventListener('DOMContentLoaded', function () {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'), 0
+  );
+  // Check if there are any nav burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
   }
 });
